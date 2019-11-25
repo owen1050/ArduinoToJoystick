6cb567f89784637f95c5aa0f35858fda6e7a0cbf
owen1050 <owen1050@gmail.com>
2019-11-25 15:55:50 -0500

    updated to match finalized electroics

--- readSerialNew.py
+++ readSerialNew.py
@@ -19,30 +19,28 @@ while 1:
         try:
                 if ser.inWaiting()>60:
                     a = str(ser.readline())         
-                    
+                    #print(a)
                     i0 = a.find("A0:")+3
                     i1 = a.find("!",i0)
                     roll = int(a[i0:i1])
+                    rog = roll
 
                     i0 = a.find("A1:")+3
                     i1 = a.find("!",i0)
                     pitch = int(a[i0:i1])
-                   
+                    pog = pitch
+
                     i0 = a.find("A2:")+3
                     i1 = a.find("!",i0)
                     yaw = int(a[i0:i1])
+                    yog = yaw
                     #print("data:\t"+str(pitch) + ":\t"+str(yaw)+":\t"+str(roll))
-                    y0 = 1797 
-                    y1 = 281* math.pow(yaw,1)
-                    y2 = -2.51* math.pow(yaw,2)
-                    y3 = 9.93 * math.pow(10,-3) * math.pow(yaw,3)
-                    y4 = -1.91 * math.pow(10,-5)* math.pow(yaw,4)
-                    y5 = 1.76 * math.pow(10,-8)* math.pow(yaw,5)
-                    y6 = -6.22 * math.pow(10,-12)* math.pow(yaw,6)
-
-                    #yaw =  (32000 - 43 * math.exp(yaw * 7.32 * 10**-3 ))/3+11000
-                    yaw = yaw * 267 - 198667
-
+                    
+                    y0 = -545201
+                    y1 = 1113* math.pow(yaw,1)
+                    y2 = -0.511 * math.pow(yaw,2)
+                    
+                    yaw = y0+y1+y2
                     yaws[yc] = yaw
                     yaw = round(np.average(yaws))
                     if(yc < len(yaws)-1):
@@ -50,31 +48,28 @@ while 1:
                     else:
                          yc = 0;
 
-                    p0 = 11797
-                    p1 = -337 * math.pow(pitch,1)
-                    p2 = 3.48 * math.pow(pitch,2)
-                    p3 = -1.68 * math.pow(10,-2) * math.pow(pitch,3)
-                    p4 = 4.37 * math.pow(10,-5) * math.pow(pitch,4)
-                    p5 = -5.54 * math.pow(10,-8) * math.pow(pitch,5)
-                    p6 = 2.68 * math.pow(10,-11) * math.pow(pitch,6)
+                    p0 = 1598
+                    p1 = -161 * math.pow(pitch,1)
+                    p2 = 2.36 * math.pow(pitch,2)
+                    p3 = -0.0137  * math.pow(pitch,3)
+                    p4 = 4.03 * math.pow(10,-5) * math.pow(pitch,4)
+                    p5 = -5.56 * math.pow(10,-8) * math.pow(pitch,5)
+                    p6 = 2.86 * math.pow(10,-11) * math.pow(pitch,6)
 
-                    pitch = p0+p1+p2+p3+p4+p5+p6
-                    pitch = round(pitch * 0.7)
+                    pitch = round(p0+p1+p2+p3+p4+p5+p6)
+                    
                     pitchs[rc] = pitch
                     pitch = round(np.average(pitchs))
                     if(pc < len(pitchs)-1):
                          pc = pc +1;
                     else:
                          pc = 0;
-                    r0 = -840
-                    r1 = 236 *math.pow(roll, 1)
-                    r2 = 2.38 * math.pow(roll, 2)
-                    r3 = -4.93 * math.pow(10,-2) * math.pow(roll, 3)
-                    r4 = 2.97 * math.pow(10,-4) * math.pow(roll, 4)
-                    r5 = -7.64 * math.pow(10,-7) *math.pow(roll, 5)
-                    r6 = 7.28 * math.pow(10,-10) * math.pow(roll, 6)
- 
-                    roll = round(r0+r1+r2+r3+r4+r5+r6)
+                    
+                    r0 = -28643
+                    r1 = 39.6 * math.pow(roll,1)
+                    r2 = 0.214 * math.pow(roll,2)
+                    
+                    roll = 32000-(r0+r1+r2)
                     rolls[rc] = roll
                     roll = round(np.average(rolls))
                     if(rc < len(rolls)-1):
@@ -82,17 +77,11 @@ while 1:
                     else:
                          rc = 0;
                     
-                    #j.data.wAxisXRot = int(v)
-                    
-                    #j.data.wAxisYRot = int(roll)
-                    #j.data.wAxisZRot = int(pitch)
-                    #print(str(roll) + " " + str(pitch))
                     j.data.wAxisZ = int(yaw)
                     j.data.wAxisX = int(roll)
                     j.data.wAxisY = int(pitch)
-                    #j.data.wSlider = int(pitch)
-                    #j.data.wDial = int(roll)
-                    #print("data:\t"+str(pitch) + ":\t"+str(yaw)+":\t"+str(roll))
+                    
+                    print("data:\t"+str(round(pitch))+ ":\t"+str(pog) + ":\t"+str(yaw)+ ":\t"+str(yog)+":\t"+str(roll)+ ":\t"+str(rog))
                     j.update()
         except: 
                 print(sys.exc_info()[0])
